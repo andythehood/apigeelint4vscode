7cfbcd5c74c4af8965652d64c2f866493985627e
Andy Hood <andyhood@google.com>
2022-12-08 16:26:51 +1100

    initial commit

---
+++ README.md
@@ -0,0 +1,33 @@
+# apigeelint
+
+Apigee Lint for VS Code
+
+## Features
+
+Runs Apigee Lint.
+
+## Requirements
+
+This section describes requirements and how to install and configure them.
+
+## Extension Settings
+
+This extension contributes the following settings:
+
+* `myExtension.enable`: Enable/disable this extension.
+* `myExtension.thing`: Set to `blah` to do something.
+
+## Known Issues
+
+No known issues.
+
+## Release Notes
+
+Releases
+
+### 0.0.1
+
+Initial release of Apigee Lint for VS Code
+
+
+
---
+++ src/extension.ts
@@ -0,0 +1,93 @@
+// The module 'vscode' contains the VS Code extensibility API
+// Import the module and reference it with the alias vscode in your code below
+
+import * as path from 'path';
+import * as fs from 'fs';
+import * as cp from 'child_process';
+import * as vscode from 'vscode';
+
+// This method is called when your extension is activated
+// Your extension is activated the very first time the command is executed
+export function activate(context: vscode.ExtensionContext) {
+
+	// Use the console to output diagnostic information (console.log) and errors (console.error)
+	// This line of code will only be executed once when your extension is activated
+	console.log('Congratulations, your extension "apigeelint" is now active!');
+
+	// The command has been defined in the package.json file
+	// Now provide the implementation of the command with registerCommand
+	// The commandId parameter must match the command field in package.json
+	let disposable = vscode.commands.registerCommand('apigeelint.runApigeelint', async (uri: vscode.Uri) => {
+		// The code you place here will be executed every time your command is executed
+		// Display a message box to the user
+		// vscode.window.showInformationMessage('Running ApigeeLint ...',);
+
+		const outputChannel: vscode.OutputChannel = vscode.window.createOutputChannel('apigee');
+		outputChannel.show(true);
+
+
+
+
+		let proxyPath: string = uri.fsPath;
+		if (!proxyPath.endsWith('apiproxy')) {
+
+			const isDirectory = (source: fs.PathLike) => fs.lstatSync(source).isDirectory();
+
+			const getDirectories = (source: string) =>
+				fs.readdirSync(source).map(name => path.join(source, name)).filter(isDirectory);
+
+			outputChannel.appendLine(getDirectories(proxyPath)[0]);
+
+			while (!(proxyPath = getDirectories(proxyPath)[0]).endsWith('apiproxy')) {
+			}
+		}
+		outputChannel.appendLine("Running Apigeelint on " + proxyPath);
+
+
+		let formatter: string = vscode.workspace.getConfiguration('apigeelint')!.get('formatter')!;
+		outputChannel.appendLine('INFO: Formatter: ' + formatter);
+
+		let profile: string = vscode.workspace.getConfiguration('apigeelint')!.get('profile')!;
+		outputChannel.appendLine('INFO: Profile: ' + profile);
+		outputChannel.appendLine('');
+
+
+		try {
+			const commandLine = 'apigeelint -s . -f ' + formatter + ' --profile ' + profile;
+
+			const { stdout, stderr } = await exec(commandLine, { cwd: proxyPath });
+			if (stderr && stderr.length > 0) {
+				outputChannel.appendLine(stderr);
+			}
+			if (stdout && stdout.length > 0) {
+				outputChannel.appendLine(stdout);
+			}
+		}
+		catch (err: any) {
+			if (err.stderr) {
+				outputChannel.appendLine(err.stderr);
+			}
+			if (err.stdout) {
+
+				outputChannel.appendLine(err.stdout);
+			}
+		}
+	});
+
+	context.subscriptions.push(disposable);
+}
+
+function exec(command: string, options: cp.ExecOptions): Promise<{ stdout: string; stderr: string }> {
+	return new Promise<{ stdout: string; stderr: string }>((resolve, reject) => {
+		cp.exec(command, options, (error, stdout, stderr) => {
+			if (error) {
+				reject({ error, stdout, stderr });
+			}
+			resolve({ stdout, stderr });
+		});
+	});
+}
+
+
+// This method is called when your extension is deactivated
+export function deactivate() { }
