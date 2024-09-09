dc1f4eae5dd48f59a75b1fce21a7ec3a05c45633
Andy Hood <andyhood@google.com>
2024-09-10 09:27:24 +1000

    path fix for 0.0.2

--- README.md
+++ README.md
@@ -10,8 +10,6 @@ To use, right-click on the apiproxy folder in the bundle and select 'Run Apigeel
 
 ![Screenshot](https://raw.githubusercontent.com/andythehood/apigeelint4vscode/main/images/apigeelint4vscode.png)
 
-
-
 ## Requirements
 
 This extension requires apigeelint to be installed. This can be installed using the command:
@@ -21,12 +19,13 @@ npm install --location=global apigeelint
 See https://www.npmjs.com/package/apigeelint for additional details.
 
 ## Extension Settings
+
 This extension contributes the following settings:
 
-* `apigeelint.formatter`: The report formatter to use (default: visualstudio.js)
-* `apigeelint.profile`: The Apigee proxy profile type: ApigeeX or ApigeeEdge (default: apigeex)
-* `apigeelint.externalPluginsDirectory`: Full path to an external plugins directory (default: none)
-* `apigeelint.excludedTests`: Comma separated list of tests to exclude, e.g. ST001 (default: none)
+- `apigeelint.formatter`: The report formatter to use (default: visualstudio.js)
+- `apigeelint.profile`: The Apigee proxy profile type: ApigeeX or ApigeeEdge (default: apigeex)
+- `apigeelint.externalPluginsDirectory`: Full path to an external plugins directory (default: none)
+- `apigeelint.excludedTests`: Comma separated list of tests to exclude, e.g. ST001 (default: none)
 
 ## Known Issues
 
@@ -36,9 +35,10 @@ No known issues.
 
 Releases
 
+### 0.0.2
+
+Fix for spaces in path of apiproxy
+
 ### 0.0.1
 
 Initial release of Apigeelint for VS Code
-
-
-
--- src/extension.ts
+++ src/extension.ts
@@ -1,123 +1,156 @@
 // The module 'vscode' contains the VS Code extensibility API
 // Import the module and reference it with the alias vscode in your code below
 
-import * as path from 'path';
-import * as fs from 'fs';
-import * as cp from 'child_process';
-import * as vscode from 'vscode';
-
+import * as path from "path";
+import * as fs from "fs";
+import * as cp from "child_process";
+import * as vscode from "vscode";
 
 let outputChannel: vscode.OutputChannel;
 
 // This method is called when your extension is activated
 // Your extension is activated the very first time the command is executed
 export function activate(context: vscode.ExtensionContext) {
+  // Use the console to output diagnostic information (console.log) and errors (console.error)
+  // This line of code will only be executed once when your extension is activated
+  console.log('Congratulations, your extension "apigeelint" is now active!');
 
-	// Use the console to output diagnostic information (console.log) and errors (console.error)
-	// This line of code will only be executed once when your extension is activated
-	console.log('Congratulations, your extension "apigeelint" is now active!');
+  outputChannel = vscode.window.createOutputChannel("Apigeelint");
 
-	outputChannel = vscode.window.createOutputChannel('Apigeelint');
+  // The command has been defined in the package.json file
+  // Now provide the implementation of the command with registerCommand
+  // The commandId parameter must match the command field in package.json
+  let disposable = vscode.commands.registerCommand(
+    "apigeelint.runApigeelint",
+    async (uri: vscode.Uri) => {
+      // The code you place here will be executed every time your command is executed
+      // Display a message box to the user
+      // vscode.window.showInformationMessage('Running ApigeeLint ...',);
 
+      outputChannel.show(true);
 
-	// The command has been defined in the package.json file
-	// Now provide the implementation of the command with registerCommand
-	// The commandId parameter must match the command field in package.json
-	let disposable = vscode.commands.registerCommand('apigeelint.runApigeelint', async (uri: vscode.Uri) => {
-		// The code you place here will be executed every time your command is executed
-		// Display a message box to the user
-		// vscode.window.showInformationMessage('Running ApigeeLint ...',);
+      let proxyPath: string = uri.fsPath;
+      if (!proxyPath.endsWith("apiproxy")) {
+        const isDirectory = (source: fs.PathLike) =>
+          fs.lstatSync(source).isDirectory();
 
-		outputChannel.show(true);
+        const getDirectories = (source: string) =>
+          fs
+            .readdirSync(source)
+            .map((name) => path.join(source, name))
+            .filter(isDirectory);
 
-		let proxyPath: string = uri.fsPath;
-		if (!proxyPath.endsWith('apiproxy')) {
+        // outputChannel.appendLine(getDirectories(proxyPath)[0]);
 
-			const isDirectory = (source: fs.PathLike) => fs.lstatSync(source).isDirectory();
+        while (
+          !(proxyPath = getDirectories(proxyPath)[0]).endsWith("apiproxy")
+        ) {}
+      }
+      outputChannel.appendLine("INFO: Running Apigeelint on " + proxyPath);
 
-			const getDirectories = (source: string) =>
-				fs.readdirSync(source).map(name => path.join(source, name)).filter(isDirectory);
+      let configuration = vscode.workspace.getConfiguration("apigeelint");
 
-			// outputChannel.appendLine(getDirectories(proxyPath)[0]);
+      let formatter: string = configuration!.get("formatter")!;
+      outputChannel.appendLine("INFO: Formatter: " + formatter);
 
-			while (!(proxyPath = getDirectories(proxyPath)[0]).endsWith('apiproxy')) {
-			}
-		}
-		outputChannel.appendLine('INFO: Running Apigeelint on ' + proxyPath);
+      let profile: string = configuration!.get("profile")!;
+      outputChannel.appendLine("INFO: Profile: " + profile);
 
+      let externalPluginsDirectory: string = configuration!.get(
+        "externalPluginsDirectory",
+      )!;
+      outputChannel.appendLine(
+        "INFO: External Plugins Directory: " + externalPluginsDirectory,
+      );
 
-		let configuration = vscode.workspace.getConfiguration('apigeelint');
+      let excludedTests: string = configuration!.get("excludedTests")!;
+      excludedTests.replace(/\s/g, "");
+      outputChannel.appendLine("INFO: Excluded Tests: " + excludedTests);
 
-		let formatter: string = configuration!.get('formatter')!;
-		outputChannel.appendLine('INFO: Formatter: ' + formatter);
+      outputChannel.appendLine("");
 
-		let profile: string = configuration!.get('profile')!;
-		outputChannel.appendLine('INFO: Profile: ' + profile);
+      try {
+        let commandLine = `apigeelint -s '${proxyPath}'`;
+        commandLine += " -f " + formatter;
+        commandLine += " --profile " + profile;
+        commandLine += externalPluginsDirectory
+          ? ` -x '${externalPluginsDirectory}'`
+          : "";
+        commandLine += excludedTests ? " -e " + excludedTests : "";
 
-		let externalPluginsDirectory: string = configuration!.get('externalPluginsDirectory')!;
-		outputChannel.appendLine('INFO: External Plugins Directory: ' + externalPluginsDirectory);
+        const {stdout, stderr} = await exec(commandLine, {});
+        // const {stdout, stderr} = await exec(commandLine, {cwd: proxyPath});
+        if (stderr && stderr.length > 0) {
+          outputChannel.appendLine(stderr);
+        }
+        if (stdout && stdout.length > 0) {
+          if (
+            ["visualstudio.js", "stylish.js", "table.js", "unix.js"].includes(
+              formatter,
+            )
+          ) {
+            const proxyPathParent = proxyPath.slice(
+              0,
+              proxyPath.lastIndexOf(path.sep),
+            );
+            const lines: string[] = stdout.split(/\n/);
+            lines.forEach((line) => {
+              outputChannel.appendLine(
+                (line.startsWith(path.sep + "apiproxy")
+                  ? proxyPathParent
+                  : "") + line,
+              );
+            });
+          } else {
+            outputChannel.appendLine(stdout);
+          }
+        }
+      } catch (err: any) {
+        if (err.stderr && err.stderr.length > 0) {
+          outputChannel.appendLine(err.stderr);
+        }
+        if (err.stdout && err.stdout.length > 0) {
+          if (
+            ["visualstudio.js", "stylish.js", "table.js", "unix.js"].includes(
+              formatter,
+            )
+          ) {
+            const proxyPathParent = proxyPath.slice(
+              0,
+              proxyPath.lastIndexOf(path.sep),
+            );
+            const lines: string[] = err.stdout.split(/\n/);
+            lines.forEach((line) => {
+              outputChannel.appendLine(
+                (line.startsWith(path.sep + "apiproxy")
+                  ? proxyPathParent
+                  : "") + line,
+              );
+            });
+          } else {
+            outputChannel.appendLine(err.stdout);
+          }
+        }
+      }
+    },
+  );
 
-		let excludedTests: string = configuration!.get('excludedTests')!;
-		excludedTests.replace(/\s/g, "");
-		outputChannel.appendLine('INFO: Excluded Tests: ' + excludedTests);
-
-		outputChannel.appendLine('');
-
-		try {
-			let commandLine = 'apigeelint -s .';
-			commandLine += ' -f ' + formatter;
-			commandLine += ' --profile ' + profile;
-			commandLine += externalPluginsDirectory ? ' -x ' + externalPluginsDirectory : '';
-			commandLine += excludedTests ? ' -e ' + excludedTests : '';
-
-			const { stdout, stderr } = await exec(commandLine, { cwd: proxyPath });
-			if (stderr && stderr.length > 0) {
-				outputChannel.appendLine(stderr);
-			}
-			if (stdout && stdout.length > 0) {
-				if (['visualstudio.js', 'stylish.js', 'table.js', 'unix.js'].includes(formatter)) {
-					const proxyPathParent = proxyPath.slice(0, proxyPath.lastIndexOf(path.sep));
-					const lines: string[] = stdout.split(/\n/);
-					lines.forEach((line) => {
-						outputChannel.appendLine((line.startsWith(path.sep + 'apiproxy') ? proxyPathParent : '') + line);
-					});
-				} else {
-					outputChannel.appendLine(stdout);
-				}
-			}
-		}
-		catch (err: any) {
-			if (err.stderr && err.stderr.length > 0) {
-				outputChannel.appendLine(err.stderr);
-			}
-			if (err.stdout && err.stdout.length > 0) {
-				if (['visualstudio.js', 'stylish.js', 'table.js', 'unix.js'].includes(formatter)) {
-					const proxyPathParent = proxyPath.slice(0, proxyPath.lastIndexOf(path.sep));
-					const lines: string[] = err.stdout.split(/\n/);
-					lines.forEach((line) => {
-						outputChannel.appendLine((line.startsWith(path.sep + 'apiproxy') ? proxyPathParent : '') + line);
-					});
-				} else {
-					outputChannel.appendLine(err.stdout);
-				}
-			}
-		}
-	});
-
-	context.subscriptions.push(disposable);
+  context.subscriptions.push(disposable);
 }
 
-const exec = (command: string, options: cp.ExecOptions): Promise<{ stdout: string; stderr: string }> => {
-	return new Promise<{ stdout: string; stderr: string }>((resolve, reject) => {
-		cp.exec(command, options, (error, stdout, stderr) => {
-			if (error) {
-				reject({ error, stdout, stderr });
-			}
-			resolve({ stdout, stderr });
-		});
-	});
+const exec = (
+  command: string,
+  options: cp.ExecOptions,
+): Promise<{stdout: string; stderr: string}> => {
+  return new Promise<{stdout: string; stderr: string}>((resolve, reject) => {
+    cp.exec(command, options, (error, stdout, stderr) => {
+      if (error) {
+        reject({error, stdout, stderr});
+      }
+      resolve({stdout, stderr});
+    });
+  });
 };
 
-
 // This method is called when your extension is deactivated
-export function deactivate() { }
+export function deactivate() {}
